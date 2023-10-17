7a2a146e5ff44cf4d77b6a8dabbf0ac2e391dde5
Sebastian Wiesner <sebastian@swsnr.de>
2023-10-17 08:46:45 +0200

    Delete defined property in disable()
    This removes the installed descriptor on the extension manager instance
    and makes access to updatesEnabled call through to the original
    prototype property again.
    
    To simplify this and avoid that very concrete typescript types get
    in our way of "delete" we abstract this into a PropertyManager class,
    similar to the built-in InjectionManager which doesn't handle properties
    unfortunately.

--- extension.ts
+++ extension.ts
@@ -20,33 +20,88 @@
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
+/**
+ * A property being tracked by a PropertyManager.
+ */
+interface TrackedProperty {
+  /** The object the property was defined on. */
+  readonly object: unknown;
+  /** The property key. */
+  readonly key: PropertyKey;
+}
+
+/**
+ * Track and delete defined properties of objects.
+ */
+class PropertyManager {
+  private trackedProperties: TrackedProperty[] = [];
+
+  /**
+   * Define a property on an object, like `Object.defineProperty`.
+   *
+   * However, keep track of the defined property, and delete it again in `reset()`.
+   * To ensure that this works, this function enforces that `configurable` is
+   * `true` on the defined property.
+   *
+   * @param object The object to the define to property on
+   * @param key The property key
+   * @param descriptor The property descriptor
+   */
+  defineProperty(
+    object: unknown,
+    key: PropertyKey,
+    descriptor: PropertyDescriptor,
+  ): void {
+    this.trackedProperties.push({ object, key });
+    Object.defineProperty(object, key, {
+      // We must enforce that the property be configurable; otherwise we can't delete it again
+      configurable: true,
+      ...descriptor,
+    });
+  }
+
+  /**
+   * Clear all tracked properties.
+   *
+   * For every property tracked by this class, delete the property on its object.
+   */
+  clear() {
+    this.trackedProperties.forEach(({ object, key }) => {
+      // Deleting is the actual point here, since we're deep in monkey-patching
+      // territory, and we kinda fake the types here just to make typescript happy.
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete (object as Record<PropertyKey, unknown>)[key];
+    });
+    this.trackedProperties = [];
+  }
+}
+
+/**
+ * Disable extension updates with some monkey-patching trickery.
+ */
 export default class DisableUpdatesExtension extends Extension {
-  private updatesDisabled = false;
-  private extensionManagerWasPatched = false;
+  private readonly propertyManager = new PropertyManager();
 
   override enable(): void {
-    if (!this.extensionManagerWasPatched) {
-      this.extensionManagerWasPatched = true;
-      Object.defineProperty(Main.extensionManager, "updatesSupported", {
+    console.log(
+      `Extension ${this.metadata.uuid} enabling, monkey-patching extension Main.extensionManager.updatesSupported to disable automatic updates`,
+    );
+    this.propertyManager.defineProperty(
+      Main.extensionManager,
+      "updatesSupported",
+      {
         get: () => {
-          if (this.updatesDisabled) {
-            console.log(`Extension updates disabled by ${this.metadata.uuid}`);
-            return false;
-          } else {
-            return (
-              (Object.getOwnPropertyDescriptor(
-                Object.getPrototypeOf(Main.extensionManager),
-                "updatesDisabled",
-              )?.get?.bind(Main.extensionManager)() as boolean | undefined) ??
-              false
-            );
-          }
+          console.log(`Extension updates disabled by ${this.metadata.uuid}`);
+          return false;
         },
-      });
-    }
+      },
+    );
   }
 
   override disable(): void {
-    this.updatesDisabled = false;
+    console.log(
+      `Extension ${this.metadata.uuid} disabled, resetting monkey-patched properties`,
+    );
+    this.propertyManager.clear();
   }
 }
