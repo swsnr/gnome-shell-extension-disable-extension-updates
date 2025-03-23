e95e932c3cc86cf450e15f6991c439fa8e2240b4
Sebastian Wiesner <sebastian@swsnr.de>
2025-03-23 13:40:13 +0100

    Cut excess infrastructure
    Write directly in JS, and remove all the extra infra around typescript
    compilation and complicated packing.
    
    We can now just pack the literal JS file, and call it a day.
    
    As a side effect, prepare for GNOME 48 by updating typings.

---
+++ extension.js
@@ -0,0 +1,134 @@
+// Copyright Sebastian Wiesner <sebastian@swsnr.de>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0.If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Alternatively, the contents of this file may be used under the terms
+// of the GNU General Public License Version 2 or later, as described below:
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// @ts-check
+
+/// <reference path="gnome-shell.d.ts" />
+
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+
+/**
+ * An abstract class representing a destructible extension.
+ *
+ * This class handles the infrastructure for enabling and disabling the
+ * extension; implementations only need to provide initialization.
+ *
+ * @typedef {{destroy: () => void}} Destructible
+ */
+export class DestructibleExtension extends Extension {
+  /**
+   * Destructible for the enabled extension, or null if the extension is not enabled.
+   *
+   * @type {Destructible | null}
+   */
+  #enabledExtension = null;
+
+  /**
+   * The version of this extension, as extracted from metadata.
+   *
+   * @type {string}
+   */
+  get version() {
+    return this.metadata["version-name"] ?? "n/a";
+  }
+
+  /**
+   * Initialize this extension.
+   *
+   * @return {Destructible} An object to destroy when tearing down the extension.
+   */
+  initialize() {
+    throw new Error("Must implement initialize");
+  }
+
+  /**
+   * Enable this extension.
+   *
+   * If not already enabled, call `initialize` and keep track its allocated resources.
+   *
+   * @override
+   */
+  enable() {
+    if (!this.#enabledExtension) {
+      console.log(`Enabling extension ${this.metadata.uuid} ${this.version}`);
+      this.#enabledExtension = this.initialize();
+      console.log(
+        `Extension ${this.metadata.uuid} ${this.version} successfully enabled`,
+      );
+    }
+  }
+
+  /**
+   * Disable this extension.
+   *
+   * If existing, destroy the allocated resources of `initialize`.
+   *
+   * @override
+   */
+  disable() {
+    console.log(`Disabling extension ${this.metadata.uuid} ${this.version}`);
+    this.#enabledExtension?.destroy();
+    this.#enabledExtension = null;
+  }
+}
+
+/**
+ * Disable extension updates with some monkey-patching trickery.
+ *
+ * We patch actual extension manager instance used by GNOME shell.  We install
+ * a new "updatesSupported" property directly on this instance object which
+ * always returns false and logs that our extension intercepted the update
+ * check.  This shadows the original property definition on the
+ * ExtensionManager prototype and thus intercepts all access to
+ * "Main.extensionManager.updatesEnabled" which is the only way the rest of
+ * the shell uses this property as there's only a single ExtensionManager
+ * instance in GNOME shell.
+ *
+ * Since the original property descriptor on the ExtensionManager prototype
+ * is left intact we can restore the old behaviour by simply deleting the
+ * property on the instance object; then the property definition on the
+ * prototype will take over again.
+ *
+ * So, when the extension is disabled, we simply clear the patched properties,
+ * i.e. delete the property on the instance object that we patched in, so that
+ * the original property definition on the prototype will take over again.
+ */
+export default class DisableUpdatesExtension extends DestructibleExtension {
+  /**
+   * @override
+   */
+  initialize() {
+    Object.defineProperty(Main.extensionManager, "updatesSupported", {
+      // We must enforce that the property be configurable; otherwise we can't delete it again
+      configurable: true,
+      get: () => {
+        console.log(`Extension updates disabled by ${this.metadata.uuid}`);
+        return false;
+      },
+    });
+    return {
+      destroy: () => {
+        // @ts-expect-error We can delete the propery because we're overwriting it above as configurable
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        delete Main.extensionManager["updatesSupported"];
+      },
+    };
+  }
+}
